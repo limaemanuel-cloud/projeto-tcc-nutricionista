4fc7bb601606a527afedddd02cbc4ff4b7a5b7c7
lucas emanuel de lima nascimento <lima.emanuel@academico.ifpb.edu.br>
2026-03-24 09:26:13 -0300

    conexão bd

--- src/app/agendamento/page.js
+++ src/app/agendamento/page.js
@@ -1,5 +1,13 @@
-export default () => (
-    <div>
-        <h1>Agendamento</h1>
-    </div>
-)
+import db from '@/lib/db';
+export default async () => {
+     const consumidores =  await db.query("select * from consumido");
+
+     return (<><h1>consumidores</h1>
+        <ol>
+             {consumidores.map(consumidores => <li>{consumidores.nome}</li>)}
+           <li></li>
+        </ol>
+    </>
+ )
+}
+ 
